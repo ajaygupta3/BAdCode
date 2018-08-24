8db2686501579761bda0baace39693e1d13d23bb
ajaygupta3 <guptaa2017@gmail.com>
2018-08-23 22:33:44 -0700

    Initial commit

---
+++ FirstJCode.java
@@ -0,0 +1,28 @@
+/* Goal: print the following using a method
+There's one thing every coder must understand:
+The System.out.println command.
+
+There's one thing every coder must understand:
+The System.out.println command.
+*/
+
+
+public class Mantra
+{
+    public static void main(String[] args)
+    {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+        System.out.println("");
+
+        
+                       
+        
+    }
+    public static void mantra()
+    {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+    }
+        
+}
